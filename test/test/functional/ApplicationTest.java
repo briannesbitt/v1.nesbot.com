26ca6857544dbdaba0a7f16db1bf53399acf8426
Brian Nesbitt <brian@nesbot.com>
2011-08-31 16:36:42 -0400

    Changed testing to use JWebUnit and the HtmlUnit plugin driver. Added a couple simple tests for the homepage.

--- test/test/functional/ApplicationTest.java
+++ test/test/functional/ApplicationTest.java
@@ -1,12 +1,29 @@
 package test.functional;
 
+import controllers.Application;
+import helpers.Globals;
 import org.junit.Test;
 
 public class ApplicationTest extends BaseFunctionalTest
 {
    @Test
-   public void testIndexPage()
+   public void testStaticPrivateCtors()
    {
-      assertPageLoadsOk("/");
+      assertPrivateNoArgsCtor(Globals.class);
+      assertPrivateNoArgsCtor(Application.class);
+   }
+   @Test
+   public void testHomeIndexPage()
+   {
+      wt.beginAt(getRoute("Application.index"));
+      wt.assertTitleEquals("Brian Nesbitt : nesbot.com");
+      wt.assertElementPresent("header");
+   }
+   @Test
+   public void testPoweredByLink()
+   {
+      wt.beginAt(getRoute("Application.index"));
+      wt.clickLinkWithExactText("powered by");
+      wt.gotoWindowByTitle("Play framework - Home");
    }
 }
--- test/test/functional/BaseFunctionalTest.java
+++ test/test/functional/BaseFunctionalTest.java
@@ -1,17 +1,58 @@
 package test.functional;
 
-import play.mvc.Http;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import helpers.Globals;
+import net.sourceforge.jwebunit.htmlunit.HtmlUnitTestingEngineImpl;
+import net.sourceforge.jwebunit.junit.WebTester;
+import org.junit.Before;
+import play.mvc.Router;
 import play.test.FunctionalTest;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public abstract class BaseFunctionalTest extends FunctionalTest
 {
-   protected Http.Response latestResponse;
+   protected WebTester wt;
+   protected BrowserVersion defaultBrowserVersion = BrowserVersion.INTERNET_EXPLORER_8;
 
-   protected void assertPageLoadsOk(String url)
+   @Before
+   public void before()
    {
-      latestResponse = GET(url);
-      assertIsOk(latestResponse);
-      assertContentType("text/html", latestResponse);
-      assertCharset("utf-8", latestResponse);
+      wt = new WebTester();
+      wt.getTestContext().setUserAgent(defaultBrowserVersion.getUserAgent());
+      if (wt.getTestingEngine() instanceof HtmlUnitTestingEngineImpl)
+      {
+         ((HtmlUnitTestingEngineImpl)wt.getTestingEngine()).setDefaultBrowserVersion(defaultBrowserVersion);
+      }
+      wt.setBaseUrl(Globals.getUrlBase());
+      wt.getTestingEngine().setIgnoreFailingStatusCodes(false);
+   }
+   protected String getRoute(String action)
+   {
+      return Router.reverse(action).url;
+   }
+   protected void assertPrivateNoArgsCtor(final Class<?> cls)
+   {
+      final Constructor<?> c = cls.getDeclaredConstructors()[0];
+      c.setAccessible(true);
+
+      try
+      {
+         final Object n = c.newInstance((Object[]) null);
+         assertNotNull(n);
+      }
+      catch (InstantiationException ex)
+      {
+         fail(ex.getMessage());
+      }
+      catch (IllegalAccessException ex)
+      {
+         fail(ex.getMessage());
+      }
+      catch (InvocationTargetException ex)
+      {
+         fail(ex.getMessage());
+      }
    }
 }
